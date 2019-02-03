dff6c06ae180d154da747fb8cf05b5b3adeb2553
zacharybys <zachary.bys@gmail.com>
2019-02-03 01:52:27 -0500

    Added AR menu with endpoint calls

---
+++ Assets/scripts/AddtoCart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddtoCart : MonoBehaviour
+{
+    public TextMeshProUGUI infoText;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+   public void UpdateText()
+    {
+        infoText.text = "Testing Add";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- back-end/app.py
+++ back-end/app.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import pyodbc
+
+server = 'pay-r.database.windows.net:1433'
+database = 'pay-r'
+username = 'pay-r'
+password = 'Party123'
+driver= '{ODBC Driver 13 for SQL Server}'
+
 app = Flask(__name__)
 
 @app.route('/')
