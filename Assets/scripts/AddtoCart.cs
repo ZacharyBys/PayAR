c150257c74f85e1b4284791bca83cd3624e88c75
zacharybys <zachary.bys@gmail.com>
2019-02-03 04:24:45 -0500

    Added PUT request scripts

--- Assets/scripts/AddtoCart.cs
+++ Assets/scripts/AddtoCart.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections;
+using System.Net;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -15,7 +18,23 @@ public class AddtoCart : MonoBehaviour
 
    public void UpdateText()
     {
-        infoText.text = "Testing Add";
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://178.128.229.75:5000/carts/1");
+        request.Method = "PUT";
+        request.ContentType = "application/json";
+
+        using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+        {
+            string json = "{\"product_id\":\"1\"}";
+
+            streamWriter.Write(json);
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        infoText.text = jsonResponse;
     }
     // Update is called once per frame
     void Update()
